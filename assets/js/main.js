2fcf53665ad8dbfd9d86a3984aa3891f474b3a64
Thiago Xanhenha <tjbarbosa.dev@gmail.com>
2021-02-08 09:52:57 -0300

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+/*
+	Concentric by Pixelarity
+	pixelarity.com | hello@pixelarity.com
+	License: pixelarity.com/license
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			wide:       [ '1281px',  '1680px' ],
+			normal:     [ '1081px',  '1280px' ],
+			narrow:     [ '961px',   '1080px' ],
+			narrower:   [ '737px',   '960px'  ],
+			mobile:     [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			offsetY: -14,
+			noOpenerFade: true
+		});
+
+	// Nav Panel.
+
+		// Title Bar.
+			$(
+				'<div id="titleBar">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+})(jQuery);
